bcd037bb8b52666088c3c2a9b98a755c81da6932
ikuchan <storyseller1980@gmail.com>
2019-01-17 15:49:13 +0900

    Lesson 6

---
+++ src/main/java/java_bootcamp/ContainerState.java
@@ -0,0 +1,62 @@
+package java_bootcamp;
+
+import com.google.common.collect.ImmutableList;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.identity.AbstractParty;
+import net.corda.core.identity.Party;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ContainerState implements ContractState {
+    private int width;
+    private int height;
+    private int depth;
+    private Party owner;
+    private Party carrier;
+    private String contents;
+
+
+
+    public ContainerState(int width, int height, int depth, String contents, Party owner, Party carrier) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+        this.contents = contents;
+        this.owner = owner;
+        this.carrier = carrier;
+
+    }
+
+    public int getWidth(){
+        return width;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public int getDepth(){
+        return depth;
+    }
+    public String getContents(){
+        return contents;
+    }
+    public Party getOwner(){
+        return owner;
+    }
+    public Party getCarrier() {
+        return carrier;
+    }
+
+    public static void main(String[]args){
+        Party jetpackImporters = null;
+        Party jetpackCarriers = null;
+        ContainerState container = new ContainerState(
+                2, 4, 2, "JETpacks", jetpackImporters,jetpackCarriers);
+    }
+
+    @NotNull
+    @Override
+    public List<AbstractParty> getParticipants() {
+        return ImmutableList.of(owner,carrier);
+    }
+}
