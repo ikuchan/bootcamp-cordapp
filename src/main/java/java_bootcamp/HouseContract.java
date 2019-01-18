c1e8e8605003cf77400e0cda51ba032260b73027
ikuchan <storyseller1980@gmail.com>
2019-01-18 13:04:52 +0900

    Finish Lesson 12

--- src/main/java/java_bootcamp/HouseContract.java
+++ src/main/java/java_bootcamp/HouseContract.java
@@ -4,6 +4,7 @@ import net.corda.core.contracts.Command;
 import net.corda.core.contracts.CommandData;
 import net.corda.core.contracts.Contract;
 import net.corda.core.contracts.ContractState;
+import net.corda.core.identity.Party;
 import net.corda.core.transactions.LedgerTransaction;
 import org.jetbrains.annotations.NotNull;
 
@@ -39,11 +40,47 @@ public class HouseContract implements Contract {
             if (houseState.getOwner().getName().getCountry().equals("Brazil"))
                 throw new IllegalArgumentException("owner must be Brazil");
 
-            //TODO("Signer")
+            //Signer
+            Party owner = houseState.getOwner();
+            PublicKey ownersKey = owner.getOwningKey();
+            if(!(requiredSigners.contains(ownersKey)))
+                throw new IllegalArgumentException("owner must sign.");
+
 
 
         }else if(commandType instanceof Transfer){
-            //TODO("transfer logic")
+            //shape
+            if(tx.getInputStates().size() != 1)
+                throw new IllegalArgumentException("Input must have one state");
+            if(tx.getOutputStates().size() != 1)
+                throw new IllegalArgumentException("Output must have one state");
+            ContractState inputState = tx.getInput(0);
+            ContractState outputState = tx.getOutput(0);
+
+            if(!(inputState instanceof HouseState))
+                throw new IllegalArgumentException("Input must be a HouseState");
+            if(!(outputState instanceof HouseState))
+                throw new IllegalArgumentException("Output must be a HouseState");
+
+            //Content
+            HouseState inputHouseState = (HouseState)inputState;
+            HouseState outputHouseState = (HouseState)outputState;
+
+            if(!(inputHouseState.getAddress().equals(outputHouseState.getAddress())))
+                throw new IllegalArgumentException("In a transfer, the address can't change");
+
+            if(!(outputHouseState.getOwner().equals(outputHouseState.getOwner())))
+                throw new IllegalArgumentException("In a transfer, the owner must change");
+
+            //signer
+            Party inputOwner = inputHouseState.getOwner();
+            Party outputOwner = outputHouseState.getOwner();
+
+            if(!(requiredSigners.contains(inputOwner.getOwningKey())))
+                throw new IllegalArgumentException("Current owner must sign transfer.");
+            if(!(requiredSigners.contains(outputOwner.getOwningKey())))
+                throw new IllegalArgumentException("Current owner must sign transfer.");
+
         }else {
             throw new IllegalArgumentException("Command type is illegal");
         }
